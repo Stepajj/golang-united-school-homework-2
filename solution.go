cb3ba09b6bfac2faf893a062be54ada599949ce7
Stepajj <89263002+Stepajj@users.noreply.github.com>
2022-02-25 21:10:22 +0100

    Update solution.go

--- solution.go
+++ solution.go
@@ -15,14 +15,13 @@ type intCustomType int
 func CalcSquare(sideLen float64, sidesNum intCustomType) float64 {
 	if sidesNum == 3 {
 		square := (sideLen * sideLen * math.Sqrt(3)) / 4
-		return square
-		//return SidesTriangle
+		return  float64(square)
 	} else if sidesNum == 4 {
 		square := sideLen * sideLen
-		return square
+		return  float64(square)
 	} else if sidesNum == 0 {
 		square := sideLen * sideLen * math.Pi
-		return square
+		return  float64(square)
 	} else {
 		return 0
 	}
